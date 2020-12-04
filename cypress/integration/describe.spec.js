108bb002b7f829712560306de8b796e12cebae6a
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-04 18:01:07 -0300

    testes/grupos/pulo de teste/apenas 1 teste

--- cypress/integration/describe.spec.js
+++ cypress/integration/describe.spec.js
@@ -1,3 +1,21 @@
 /// <reference types="cypress" />
+//esse reference faz aparecer as sugestoes de comandos do cypress
 
-it
+describe('Should group tests...', () => {
+
+    describe.skip('Should group more specific tests...', () => {
+        it('A specific test...', () => {
+
+        })        
+    })
+
+    describe('Should group more specific tests...', () => {
+        it.only('A specific test2...', () => {
+
+        })        
+    })
+
+    it('A internal test...', () => {
+
+    })
+})
