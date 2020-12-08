7070b103f0ba926f0cf4e7a60c652a543af74e72
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-08 18:18:25 -0300

    checkboxes

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -25,7 +25,7 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
-    it.only('Text fields', () => {
+    it('Text fields', () => {
         cy.get('#formNome').type('Cypress Test')
         cy.get('#formNome').should('have.value', 'Cypress Test')
 
@@ -43,6 +43,27 @@ describe('Work with basic elements', () => {
             .clear()  
             .type('Erro{selectall}acerto', {delay:100})   
             .should('have.value', 'acerto')       
+    })
+
+    it('RadioButton', () => {
+
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+
+        cy.get('#formSexoMasc').should('not.be.checked')
+
+        cy.get("[name='formSexo']").should('have.length', 2)
+
+    })
+
+    it.only('Checkbox', () => {
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked')
+        cy.get("[name='formComidaFavorita']").click({multiple: true})
+        cy.get('#formComidaPizza').should('not.be.checked')
+        cy.get('#formComidaVegetariana').should('be.checked')
     })
 
 })
