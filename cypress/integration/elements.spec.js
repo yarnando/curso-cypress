12e9c658ef38e371620251724b7517bdbf5d1d3d
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-08 18:36:24 -0300

    combos

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -57,13 +57,29 @@ describe('Work with basic elements', () => {
 
     })
 
-    it.only('Checkbox', () => {
+    it('Checkbox', () => {
         cy.get('#formComidaPizza')
             .click()
             .should('be.checked')
         cy.get("[name='formComidaFavorita']").click({multiple: true})
         cy.get('#formComidaPizza').should('not.be.checked')
         cy.get('#formComidaVegetariana').should('be.checked')
+    })
+
+    it('Combo', () => {
+
+        cy.get('[data-test=dataEscolaridade]')
+            .select('2o grau completo')
+            .should('have.value', '2graucomp')
+
+        cy.get('[data-test=dataEscolaridade]')
+            .select('1graucomp')
+            .should('have.value', '1graucomp')
+    })
+
+    it.only('Combo multiplo', () => {
+        cy.get('[data-testid=dataEsportes]')
+            .select(['natacao', 'Corrida', 'nada'])
     })
 
 })
