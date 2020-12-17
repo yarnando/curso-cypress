cb3b537b2c857b81894b942b1705adde0513d466
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-17 18:01:42 -0300

    FINALIZAÇÃO DO TESTE FUNCIONAL

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -19,7 +19,7 @@ describe('Should test at a functional level', () => {
 
     it('Should update and account', () => {
         cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta de teste')).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')        
@@ -33,5 +33,30 @@ describe('Should test at a functional level', () => {
         cy.get(loc.CONTAS.BTN_SALVAR).click();
         cy.get(loc.MESSAGE).should('contain', 'code 400')
     })
+
+    it('Should create a transaction', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc');
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123');
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter');
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada');
+        cy.get(loc.MOVIMENTACAO.STATUS).click();
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click();
+        cy.get(loc.MESSAGE).should('contain', 'sucesso');
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7);
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
+    });
+
+    it('Should get balance', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
+    });
+
+    it('Should remove a transaction', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('Desc')).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso');
+    });
 
 })
