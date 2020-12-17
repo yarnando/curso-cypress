4fd5329b46508adf45d6645b473198ffd2eab41e
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-17 14:02:53 -0300

    reutilização de comando

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -2,6 +2,8 @@
 
 import loc from '../../support/locators'
 
+import '../../support/commandsContas'
+
 describe('Should test at a functional level', () => {
 
     before(() => {
@@ -10,16 +12,13 @@ describe('Should test at a functional level', () => {
     })
 
     it('Should create an account', () => {
-         cy.get(loc.MENU.SETTINGS).click()
-         cy.get(loc.MENU.CONTAS).click()
-         cy.get(loc.CONTAS.NOME).type('Conta de teste')
-         cy.get(loc.CONTAS.BTN_SALVAR).click()
-         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
+        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
     it('Should update and account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
+        cy.acessarMenuConta()
         cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
