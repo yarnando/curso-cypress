445909451c2fd77ac7d7e61465f34daefed7a27a
Fernando Silva <fernandosilva.smsrio@gmail.com>
2020-12-21 14:39:36 -0300

    limpar localstorage

--- cypress/integration/barriga/frontend.spec.js
+++ cypress/integration/barriga/frontend.spec.js
@@ -6,7 +6,40 @@ import '../../support/commandsContas'
 
 describe('Should test at a functional level', () => {
 
+    after(() => {
+        cy.clearLocalStorage()
+    })
+
     before(() => {
+        cy.server()
+        cy.route({
+					method: "POST",
+					url: "/signin",
+					response: {
+						id: 1000,
+						nome: "Usuario falso",
+						token:
+							"Uma string muito grande que nao deveria ser aceita mas vai.",
+					},
+                }).as("signin")
+
+        cy.route({
+					method: "GET",
+					url: "/saldo",
+					response: [
+						{
+							conta_id: 999,
+							conta: "Carteira",
+							saldo: "100.00",
+						},
+						{
+							conta_id: 9909,
+							conta: "Banco",
+							saldo: "10000000.00",
+						}
+					],
+                }).as("saldo")
+            
         cy.login('bbbb@gmail.com', '123')
     })
 
